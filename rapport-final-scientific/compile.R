ab084e0cb14e53ea30a86824f0daa523c7d9e317
Ghislain Vieilledent <ghislain.vieilledent@cirad.fr>
2018-02-20 12:03:44 +0100

    Use of bookdown

---
+++ rapport-final-scientific/compile.R
@@ -0,0 +1,22 @@
+#!/usr/bin/Rscript
+
+# ==============================================================================
+# author          :Ghislain Vieilledent
+# email           :ghislain.vieilledent@cirad.fr, ghislainv@gmail.com
+# web             :https://ghislainv.github.io
+# license         :CC-BY-SA 4.0
+# ==============================================================================
+
+# Libraries
+library(bookdown)
+library(knitr)
+library(kableExtra)
+
+# Working directory
+setwd("rapport-final-scientific")
+
+# Bookdown
+bookdown::render_book("index.Rmd", output_format="pdf_document2")
+                      #output_file="report.Rmd", output_dir="../docs")
+bookdown::render_book("index.Rmd", output_format="gitbook")
+                      #output_file="report.Rmd", output_dir="../docs")
