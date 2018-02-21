05ee0f02dae6c59ba943b63d4eaefc9792844ce3
Ghislain Vieilledent <ghislain.vieilledent@cirad.fr>
2018-02-21 16:53:50 +0100

    Add preface

--- rapport-final-scientific/compile_book.R
+++ rapport-final-scientific/compile_book.R
@@ -21,12 +21,15 @@ options(knitr.table.format="latex")
 pdf_format <- bookdown::pdf_document2(citation_package="natbib", fig_caption=TRUE, keep_tex=FALSE,
                                       latex_engine="pdflatex", number_sections=FALSE, toc=FALSE,
                                       includes=list(in_header="header.tex", before_body="doc_prefix.tex"))
-params <- list(set_title="")
+params <- list(title="",author="",date="")
 bookdown::render_book("index.Rmd", output_format=pdf_format)
 
 # html
 # Don't indicate output_format to take into account YAML options
 options(knitr.table.format="html")
+# Dynamic YAML options
 title_html <- "Scientific result report for the FRB BioSceneMada project"
-params <- list(set_title=title_html)
+author_html <- "G. Vieilledent, T. F. Allnutt, C. Grinand, M. Pedrono, A. Razafimpahanana and J.-R. Rakotoarijaona"
+data_html <- format(Sys.time(), "%d %B, %Y")
+params <- list(title=title_html,author=author_html ,date=data_html)
 bookdown::render_book("index.Rmd")
